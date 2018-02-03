34ca20f1f4cb0ce0bb6b8d52cb1a31732aab9e26
Andrey E <evd0c89@gmail.com>
2018-02-03 12:05:58 +0300

    Добавил библиотеку с интерфейсом решателя

---
+++ SquareRootLib/IGetSquareRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRootLib
+{
+    public enum SquareRootResultEnum { NoRoots, OneRoot, TwoRoots, InvalidCoef}
+    public interface IGetSquareRoot
+    {
+        SquareRootResultEnum GetSquareRoot(double a, double b, double c, out List<double> roots);
+    }
+}
